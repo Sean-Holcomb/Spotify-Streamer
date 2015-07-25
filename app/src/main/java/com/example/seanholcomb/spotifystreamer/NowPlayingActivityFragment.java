d40b8a5bff64140ec3ec8453aab8fb930cd31707
Sean Holcomb <seanholcomb@gmail.com>
2015-07-24 17:25:53 -0700

    Implement trackbar functionality.

--- app/src/main/java/com/example/seanholcomb/spotifystreamer/NowPlayingActivityFragment.java
+++ app/src/main/java/com/example/seanholcomb/spotifystreamer/NowPlayingActivityFragment.java
@@ -3,12 +3,15 @@ package com.example.seanholcomb.spotifystreamer;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,6 +27,7 @@ import java.util.List;
  */
 public class NowPlayingActivityFragment extends Fragment {
 
+    private final int SONG_LENGTH=30;
     private ArtistParcel mParcel;
     private List<String> images = new ArrayList<>();
     private List<String> albumNames = new ArrayList<>();
@@ -33,9 +37,17 @@ public class NowPlayingActivityFragment extends Fragment {
     private TextView album_textView;
     private TextView track_textView;
     private ImageView albumArt_imageView;
+    private SeekBar seekBar;
+    private TextView amountPlayed;
+    private TextView amountLeft;
+    ImageButton playButton;
+
     private int mPosition=0;
     private MediaPlayer mediaPlayer;
     private List<String> mTracks;
+    private Handler handler = new Handler();
+    private Boolean touchLock = false;
+
 
 
     public NowPlayingActivityFragment() {
@@ -84,20 +96,50 @@ public class NowPlayingActivityFragment extends Fragment {
         album_textView=(TextView)rootView.findViewById(R.id.album);
         track_textView=(TextView)rootView.findViewById(R.id.track);
         albumArt_imageView=(ImageView)rootView.findViewById(R.id.album_art);
+        seekBar=(SeekBar)rootView.findViewById(R.id.seekbar);
+        amountPlayed=(TextView)rootView.findViewById(R.id.timePlayed);
+        amountLeft=(TextView)rootView.findViewById(R.id.timeLeft);
         ImageButton preButton=(ImageButton)rootView.findViewById(R.id.back_button);
         ImageButton nextButton =(ImageButton)rootView.findViewById(R.id.next_button);
-        ImageButton playButton = (ImageButton)rootView.findViewById(R.id.play_button);
+        playButton = (ImageButton)rootView.findViewById(R.id.play_button);
         bindView(mPosition);
         playMusic(mTracks.get(mPosition));
+
+
+        OnSeekBarChangeListener listener = new OnSeekBarChangeListener(){
+
+
+            @Override
+            public void onProgressChanged (SeekBar seekBar, int progress, boolean fromUser){
+                if (touchLock) {
+                    mediaPlayer.seekTo(progress*1000);
+                }
+                //amountPlayed.setText("0:" + progress);
+                //amountLeft.setText("0:"+(SONG_LENGTH-progress));
+            }
+
+            @Deprecated
+            public void onStartTrackingTouch (SeekBar seekBar){
+                touchLock =true;
+            }
+
+            @Override
+            public void onStopTrackingTouch (SeekBar seekBar){
+                touchLock =false;
+            }
+        };
+        seekBar.setMax(SONG_LENGTH);
+        seekBar.setOnSeekBarChangeListener(listener);
 
         playButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 if (mediaPlayer.isPlaying()) {
                     mediaPlayer.pause();
-                    //playButton.setImageDrawable();
+                    playButton.setImageResource(R.drawable.ic_media_play);
 
                 } else {
                     mediaPlayer.start();
+                    playButton.setImageResource(R.drawable.ic_media_pause);
 
 
                 }
@@ -151,8 +193,59 @@ public class NowPlayingActivityFragment extends Fragment {
             mediaPlayer.setDataSource(url);
             mediaPlayer.prepareAsync();
             mediaPlayer.start();
+            syncSeekBar();
         }catch(IOException except){
             Toast.makeText(getActivity(), "Connection Problem", Toast.LENGTH_SHORT).show();
         }
     }
+
+    public void syncSeekBar() {
+        int position = mediaPlayer.getCurrentPosition() / 1000;
+        seekBar.setProgress(position);
+        if (position<10) {
+            amountPlayed.setText("0:0" + String.valueOf(position));
+            amountLeft.setText("-0:" + String.valueOf(30 - position));
+        } else if (position>20) {
+            amountPlayed.setText("0:" + String.valueOf(position));
+            amountLeft.setText("-0:0" + String.valueOf(30 - position));
+        } else{
+            amountPlayed.setText("0:" + String.valueOf(position));
+            amountLeft.setText("-0:" + String.valueOf(30 - position));
+        }
+        handler.postDelayed(runnable, 1000);
+    }
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            syncSeekBar();
+        }
+    };
+
+
+/*
+    public class SpotifyService extends Service implements MediaPlayer.OnPreparedListener {
+        private static final String ACTION_PLAY = "com.example.action.PLAY";
+        MediaPlayer mediaPlayer = null;
+
+        @Override
+        public IBinder onBind(Intent intent) {
+            return null;
+        }
+
+        public int onStartCommand(Intent intent, int flags, int startId) {
+
+            if (intent.getAction().equals(ACTION_PLAY)) {
+                mediaPlayer = new MediaPlayer();
+                mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                mediaPlayer.setOnPreparedListener(this);
+                mediaPlayer.prepareAsync(); // prepare async to not block main thread
+            }
+        }
+
+        // Called when MediaPlayer is ready
+        public void onPrepared(MediaPlayer player) {
+            player.start();
+        }
+    }*/
 }
